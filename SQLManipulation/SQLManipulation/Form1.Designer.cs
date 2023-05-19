359f2121c9ee5f7112659a0664e3a82bdc24a97e
Syomai <126589688+Syomaii@users.noreply.github.com>
2023-05-19 21:59:02 +0800

    Add files via upload

---
+++ SQLManipulation/SQLManipulation/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace SQLManipulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEAPAS = new System.Windows.Forms.Button();
+            this.btnDTS = new System.Windows.Forms.Button();
+            this.btnDSLNSM = new System.Windows.Forms.Button();
+            this.btnDPEADS = new System.Windows.Forms.Button();
+            this.btnDTSDS = new System.Windows.Forms.Button();
+            this.btnDELS = new System.Windows.Forms.Button();
+            this.btnDEMT = new System.Windows.Forms.Button();
+            this.btnDETC = new System.Windows.Forms.Button();
+            this.btnPAMT = new System.Windows.Forms.Button();
+            this.dGrid1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEAPAS
+            // 
+            this.btnEAPAS.BackColor = System.Drawing.Color.Cyan;
+            this.btnEAPAS.Location = new System.Drawing.Point(837, 121);
+            this.btnEAPAS.Name = "btnEAPAS";
+            this.btnEAPAS.Size = new System.Drawing.Size(320, 50);
+            this.btnEAPAS.TabIndex = 0;
+            this.btnEAPAS.Text = "Display Employees Above Production Average Salary";
+            this.btnEAPAS.UseVisualStyleBackColor = false;
+            this.btnEAPAS.Click += new System.EventHandler(this.btnEAPAS_Click);
+            // 
+            // btnDTS
+            // 
+            this.btnDTS.BackColor = System.Drawing.Color.Cyan;
+            this.btnDTS.Location = new System.Drawing.Point(837, 177);
+            this.btnDTS.Name = "btnDTS";
+            this.btnDTS.Size = new System.Drawing.Size(320, 46);
+            this.btnDTS.TabIndex = 1;
+            this.btnDTS.Text = "Display Total Salaries";
+            this.btnDTS.UseVisualStyleBackColor = false;
+            this.btnDTS.Click += new System.EventHandler(this.btnDTS_Click);
+            // 
+            // btnDSLNSM
+            // 
+            this.btnDSLNSM.BackColor = System.Drawing.Color.Cyan;
+            this.btnDSLNSM.Location = new System.Drawing.Point(837, 229);
+            this.btnDSLNSM.Name = "btnDSLNSM";
+            this.btnDSLNSM.Size = new System.Drawing.Size(320, 52);
+            this.btnDSLNSM.TabIndex = 2;
+            this.btnDSLNSM.Text = "Display Salary of Last Name Starting with M";
+            this.btnDSLNSM.UseVisualStyleBackColor = false;
+            this.btnDSLNSM.Click += new System.EventHandler(this.btnDSLNSM_Click);
+            // 
+            // btnDPEADS
+            // 
+            this.btnDPEADS.BackColor = System.Drawing.Color.Cyan;
+            this.btnDPEADS.Location = new System.Drawing.Point(837, 287);
+            this.btnDPEADS.Name = "btnDPEADS";
+            this.btnDPEADS.Size = new System.Drawing.Size(320, 71);
+            this.btnDPEADS.TabIndex = 3;
+            this.btnDPEADS.Text = "Display Production Employee Above Department Salary";
+            this.btnDPEADS.UseVisualStyleBackColor = false;
+            this.btnDPEADS.Click += new System.EventHandler(this.btnDPEADS_Click);
+            // 
+            // btnDTSDS
+            // 
+            this.btnDTSDS.BackColor = System.Drawing.Color.Cyan;
+            this.btnDTSDS.Location = new System.Drawing.Point(837, 364);
+            this.btnDTSDS.Name = "btnDTSDS";
+            this.btnDTSDS.Size = new System.Drawing.Size(320, 44);
+            this.btnDTSDS.TabIndex = 4;
+            this.btnDTSDS.Text = "Display Total Sales Department Salaries";
+            this.btnDTSDS.UseVisualStyleBackColor = false;
+            this.btnDTSDS.Click += new System.EventHandler(this.btnDTSDS_Click);
+            // 
+            // btnDELS
+            // 
+            this.btnDELS.BackColor = System.Drawing.Color.Cyan;
+            this.btnDELS.Location = new System.Drawing.Point(837, 414);
+            this.btnDELS.Name = "btnDELS";
+            this.btnDELS.Size = new System.Drawing.Size(320, 51);
+            this.btnDELS.TabIndex = 5;
+            this.btnDELS.Text = "Display Employee with Least Salary";
+            this.btnDELS.UseVisualStyleBackColor = false;
+            this.btnDELS.Click += new System.EventHandler(this.btnDELS_Click);
+            // 
+            // btnDEMT
+            // 
+            this.btnDEMT.BackColor = System.Drawing.Color.Cyan;
+            this.btnDEMT.Location = new System.Drawing.Point(837, 532);
+            this.btnDEMT.Name = "btnDEMT";
+            this.btnDEMT.Size = new System.Drawing.Size(320, 52);
+            this.btnDEMT.TabIndex = 6;
+            this.btnDEMT.Text = "Display Employee with Multiple Trainings";
+            this.btnDEMT.UseVisualStyleBackColor = false;
+            this.btnDEMT.Click += new System.EventHandler(this.btnDEMT_Click);
+            // 
+            // btnDETC
+            // 
+            this.btnDETC.BackColor = System.Drawing.Color.Cyan;
+            this.btnDETC.Location = new System.Drawing.Point(837, 471);
+            this.btnDETC.Name = "btnDETC";
+            this.btnDETC.Size = new System.Drawing.Size(320, 55);
+            this.btnDETC.TabIndex = 7;
+            this.btnDETC.Text = "Display Employee with Training Course";
+            this.btnDETC.UseVisualStyleBackColor = false;
+            this.btnDETC.Click += new System.EventHandler(this.btnDETC_Click);
+            // 
+            // btnPAMT
+            // 
+            this.btnPAMT.BackColor = System.Drawing.Color.Cyan;
+            this.btnPAMT.Location = new System.Drawing.Point(837, 590);
+            this.btnPAMT.Name = "btnPAMT";
+            this.btnPAMT.Size = new System.Drawing.Size(320, 54);
+            this.btnPAMT.TabIndex = 8;
+            this.btnPAMT.Text = "Display Participants of Anger Management Training";
+            this.btnPAMT.UseVisualStyleBackColor = false;
+            this.btnPAMT.Click += new System.EventHandler(this.btnPAMT_Click);
+            // 
+            // dGrid1
+            // 
+            this.dGrid1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dGrid1.BackgroundColor = System.Drawing.Color.Aquamarine;
+            this.dGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGrid1.Location = new System.Drawing.Point(43, 121);
+            this.dGrid1.Name = "dGrid1";
+            this.dGrid1.RowHeadersWidth = 51;
+            this.dGrid1.RowTemplate.Height = 24;
+            this.dGrid1.Size = new System.Drawing.Size(765, 523);
+            this.dGrid1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(314, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(558, 97);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "KOMPANYA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(1180, 679);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dGrid1);
+            this.Controls.Add(this.btnPAMT);
+            this.Controls.Add(this.btnDETC);
+            this.Controls.Add(this.btnDEMT);
+            this.Controls.Add(this.btnDELS);
+            this.Controls.Add(this.btnDTSDS);
+            this.Controls.Add(this.btnDPEADS);
+            this.Controls.Add(this.btnDSLNSM);
+            this.Controls.Add(this.btnDTS);
+            this.Controls.Add(this.btnEAPAS);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Kompanya";
+            ((System.ComponentModel.ISupportInitialize)(this.dGrid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEAPAS;
+        private System.Windows.Forms.Button btnDTS;
+        private System.Windows.Forms.Button btnDSLNSM;
+        private System.Windows.Forms.Button btnDPEADS;
+        private System.Windows.Forms.Button btnDTSDS;
+        private System.Windows.Forms.Button btnDELS;
+        private System.Windows.Forms.Button btnDEMT;
+        private System.Windows.Forms.Button btnDETC;
+        private System.Windows.Forms.Button btnPAMT;
+        private System.Windows.Forms.DataGridView dGrid1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SQLManipulation/SQLManipulation/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLManipulation
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\User\Desktop\dev\2NDYEAR\2nd SEM\appsdev\SQLManipulation\KOMPANYA.accdb");
+        }
+
+
+        private void btnDETC_Click(object sender, EventArgs e)
+        {
+            
+            string query = "SELECT EMPLOYEEFILE.EMPLOYEEIDNO, EMPLOYEEFILE.EMPLOYEEFIRSTNAME, EMPLOYEEFILE.EMPLOYEELASTNAME, TRAININGFILE.TRAININGCOURSE, EMPLOYEEFILE.EMPLOYEEDEPARTMENT FROM ((EMPLOYEEFILE INNER JOIN ATTENDANCEFILE ON EMPLOYEEFILE.EMPLOYEEIDNO = ATTENDANCEFILE.ATTENDANCETRAININGEMPLOYEEID) INNER JOIN TRAININGFILE ON ATTENDANCEFILE.ATTENDANCETRAININGCODE = TRAININGFILE.TRAININGCODE)";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            // Set the data source of the DataGridView control to the filled DataTable
+            dGrid1.DataSource = table;
+
+            // Set the column names in the DataGridView control to match the columns provided
+            dGrid1.Columns[0].HeaderText = "EMPLOYEEIDNO";
+            dGrid1.Columns[1].HeaderText = "EMPLOYEEFIRSTNAME";
+            dGrid1.Columns[2].HeaderText = "TRAININGCOURSE";
+            dGrid1.Columns[3].HeaderText = "EMPLOYEEDEPARTMENT";
+        }
+
+        private void btnDTS_Click(object sender, EventArgs e)
+        {
+            string query = "Select Sum(EMPLOYEESALARY) from EMPLOYEEFILE";
+            OleDbCommand command = new OleDbCommand(query, connection);
+            connection.Open();
+            object result = command.ExecuteScalar();
+            connection.Close();
+
+            if (result != DBNull.Value && result != null)
+            {
+                int totalSalary = Convert.ToInt32(result);
+                MessageBox.Show(string.Format($"Total Salary = {result:C}"));
+            }
+            else
+            {
+                MessageBox.Show("No data found");
+            }
+        }
+
+        private void btnDSLNSM_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT AVG(EMPLOYEESALARY) FROM EMPLOYEEFILE WHERE EMPLOYEELASTNAME LIKE 'M%'";
+            OleDbCommand command = new OleDbCommand(query, connection);
+            connection.Open();
+            object result = command.ExecuteScalar();
+            connection.Close();
+
+            if (result != DBNull.Value && result != null)
+            {
+                double aveSalary = Convert.ToDouble(result);
+                MessageBox.Show(string.Format($"Average salary of employees whose last name starts with 'M' = {aveSalary:C}"));
+            }
+            else
+            {
+                MessageBox.Show("No data found");
+            }
+        }
+
+        private void btnDPEADS_Click(object sender, EventArgs e)
+        {
+            string query = "Select EMPLOYEEIDNO as [ID NUMBER], EMPLOYEEFIRSTNAME as FIRSTNAME, EMPLOYEELASTNAME as LASTNAME, EMPLOYEESALARY as SALARY From EMPLOYEEFILE Where EMPLOYEEDEPARTMENT = 'PRODUCTION' And EMPLOYEESALARY > (SELECT AVG(EMPLOYEESALARY) FROM EMPLOYEEFILE WHERE EMPLOYEEDEPARTMENT = 'PRODUCTION')";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            dGrid1.DataSource = table;
+        }
+
+        private void btnDTSDS_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT SUM(EMPLOYEESALARY) FROM EMPLOYEEFILE WHERE EMPLOYEEDEPARTMENT = 'SALES'";
+            OleDbCommand command = new OleDbCommand(query, connection);
+            connection.Open();
+            object result = command.ExecuteScalar();
+            connection.Close();
+
+            if (result != DBNull.Value && result != null)
+            {
+                int totalSalary = Convert.ToInt32(result);
+                MessageBox.Show(string.Format($"Total salaries of SALES department employees = {totalSalary:C}"));
+            }
+            else
+            {
+                MessageBox.Show("No data found");
+            }
+        }
+
+        private void btnDELS_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT EMPLOYEEIDNO as [ID Number], EMPLOYEEFIRSTNAME + ' ' + EMPLOYEELASTNAME as [Full Name], EMPLOYEESALARY as Salary, EMPLOYEEDEPARTMENT as Department FROM EMPLOYEEFILE WHERE EMPLOYEESALARY = (SELECT MIN(EMPLOYEESALARY) FROM EMPLOYEEFILE)";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            dGrid1.DataSource = table;
+        }
+
+        private void btnEAPAS_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT EMPLOYEEIDNO, EMPLOYEEFIRSTNAME + ' ' + EMPLOYEELASTNAME as FULLNAME, EMPLOYEEDEPARTMENT, EMPLOYEESALARY FROM EMPLOYEEFILE WHERE EMPLOYEESALARY > (SELECT AVG(EMPLOYEESALARY) FROM EMPLOYEEFILE WHERE EMPLOYEEDEPARTMENT = 'PRODUCTION')";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataSet ds = new DataSet();
+            try
+            {
+                connection.Open();
+                adapter.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error {ex.Message}");
+            }
+            finally
+            {
+                connection.Close();
+            }
+            if (ds.Tables.Count > 0)
+                dGrid1.DataSource = ds.Tables[0];
+            else
+                MessageBox.Show("No tables found in DataSet");
+        }
+
+        private void btnDEMT_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT EMPLOYEELASTNAME as [Last Name], COUNT(*) as [Training Count] FROM ATTENDANCEFILE INNER JOIN EMPLOYEEFILE ON ATTENDANCEFILE.ATTENDANCETRAININGEMPLOYEEID = EMPLOYEEFILE.EMPLOYEEIDNO GROUP BY EMPLOYEELASTNAME HAVING COUNT(*) > 1";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            dGrid1.DataSource = table;
+
+            dGrid1.Columns[0].HeaderText = "Last Name";
+            dGrid1.Columns[1].HeaderText = "Training Count";
+        }
+
+        private void btnPAMT_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT EMPLOYEEFILE.EMPLOYEEFIRSTNAME + ' ' + EMPLOYEEFILE.EMPLOYEELASTNAME AS [Full Name], EMPLOYEEFILE.EMPLOYEEDEPARTMENT AS [Department] " +
+                                 "FROM ((ATTENDANCEFILE " +
+                                 "INNER JOIN EMPLOYEEFILE ON ATTENDANCEFILE.ATTENDANCETRAININGEMPLOYEEID = EMPLOYEEFILE.EMPLOYEEIDNO) " +
+                                 "INNER JOIN TRAININGFILE ON ATTENDANCEFILE.ATTENDANCETRAININGCODE = TRAININGFILE.TRAININGCODE) " +
+                                 "WHERE TRAININGFILE.TRAININGCOURSE = 'ANGER MANAGEMENT'";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(query, connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            dGrid1.DataSource = table;
+            dGrid1.Columns[0].HeaderText = "Full Name";
+            dGrid1.Columns[1].HeaderText = "Department";
+        }
+    }
+}
